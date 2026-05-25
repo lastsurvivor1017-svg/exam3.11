1d66f1c13e0ede40613f921eb28fe8d004e0b716
Madboy <lastsurvivor1017@gmail.com>
2026-05-25 19:00:35 +0500

    models Like

--- social/models.py
+++ social/models.py
@@ -22,3 +22,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user.username}"
+    
+
+
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
