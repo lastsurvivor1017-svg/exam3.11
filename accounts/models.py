f4368475bc2081a14d50ea9d5020d570bb9645d2
Madboy <lastsurvivor1017@gmail.com>
2026-05-25 12:37:55 +0500

    models User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('citizen', 'Citizen'),
+        ('driver', 'Driver'),
+        ('shop_owner', 'Shop Owner'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='citizen')
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
+    
+
