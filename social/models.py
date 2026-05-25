43d35bed4f9f4b2adda4f84a6fb959bc1485cc2d
Madboy <lastsurvivor1017@gmail.com>
2026-05-25 18:02:55 +0500

    fix the project3

--- social/models.py
+++ social/models.py
@@ -4,23 +4,10 @@ from accounts.models import User
 
 
 class Post(models.Model):
-
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     content = models.TextField()
-
-    image = models.ImageField(
-        upload_to='posts/',
-        blank=True,
-        null=True
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
+    image = models.ImageField(upload_to='posts/',blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} Post"
