3286ef796b8397be552ee6d19fc73819ae1386fd
Madboy <lastsurvivor1017@gmail.com>
2026-05-25 15:24:31 +0500

    marketplace

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Shop, Product, Cart, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Shop)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Shop(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
+    
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('delivered', 'Delivered'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.order.user.username} --> {self.price}'
+    
