c90478ff6457795e7c931f076cd34b8081b42074
Madboy <lastsurvivor1017@gmail.com>
2026-05-25 18:56:27 +0500

    models comment

--- social/models.py
+++ social/models.py
@@ -11,3 +11,14 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Post"
+    
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username}"
