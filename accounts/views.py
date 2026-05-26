ca9892cfdaa049f989620690d9a2744322086055
Madboy <lastsurvivor1017@gmail.com>
2026-05-26 21:54:17 +0500

    login

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,54 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RegisterSerializer
 from .models import User
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework import status
 
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
+class RegisterView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        full_name = request.data.get("full_name")
 
+        if User.objects.filter(email=email).exists():
+            return Response({"error": "Email already exists"}, status=400)
+
+        user = User.objects.create_user(
+            email=email,
+            password=password,
+            full_name=full_name
+        )
+
+        return Response({
+            "message": "User created successfully",
+            "user_id": user.id
+        }, status=status.HTTP_201_CREATED)
+
+
+
+User = get_user_model()
+
+
+class LoginView(APIView):
+    def post(self, request):
+
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user:
+            return Response({
+                "message": "Login successful",
+                "user": {
+                    "id": user.id,
+                    "email": user.email,
+                    "role": user.role
+                }
+            })
+
+        return Response({"error": "Invalid credentials"}, status=400)
--- config/settings.py
+++ config/settings.py
@@ -150,3 +150,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lastsurvivor1017@gmail.com'
+EMAIL_HOST_PASSWORD = 'hshu tfrg ydnt xvvw'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
