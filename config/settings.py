860a5cf9939e81a0ab32efe6056e372ec21e7742
Madboy <lastsurvivor1017@gmail.com>
2026-05-26 11:10:39 +0500

    html.css

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [
-    BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
