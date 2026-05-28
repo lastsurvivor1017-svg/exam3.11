036eee6472e26e65739c251c0d3ceddac4c5dc04
Madboy <lastsurvivor1017@gmail.com>
2026-05-28 23:32:22 +0500

    tax taxi

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('stats/', DashboardStatsView.as_view()),
-    path('news/', views.news, name='news'),
     path('taxi/', views.taxi, name='taxi')
 ]
