2a712c60ddcfe746c82801469b3f21ab32116e1e
Madboy <lastsurvivor1017@gmail.com>
2026-05-28 17:43:51 +0500

    fix the urls complaints

--- complaints/urls.py
+++ complaints/urls.py
@@ -1,15 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from .views import ComplaintViewSet
-
-router = DefaultRouter()
-
-router.register('', ComplaintViewSet)
-
-urlpatterns = router.urls
-
 from django.urls import path
 from . import views
 
+router = DefaultRouter()
+router.register('', ComplaintViewSet)
+
 urlpatterns = router.urls + [
-    path('', views.complaints_page, name='complaints'),
+    path('page/', views.complaints_page, name='complaints'),
 ]
