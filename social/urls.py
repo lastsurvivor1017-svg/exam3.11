a9d4b501744366479f929182d253c17124416d59
Madboy <lastsurvivor1017@gmail.com>
2026-05-28 17:49:09 +0500

    urls social

--- social/urls.py
+++ social/urls.py
@@ -1,14 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet,CommentViewSet,LikeViewSet
+from .views import PostViewSet, CommentViewSet, LikeViewSet
 from django.urls import path
 from . import views
 
 router = DefaultRouter()
+
 router.register('posts', PostViewSet)
 router.register('comments', CommentViewSet)
 router.register('likes', LikeViewSet)
-urlpatterns = router.urls
 
-urlpatterns = [
-    path('social/', views.social, name='social')
+urlpatterns = router.urls + [
+    path('page/', views.social, name='social'),
 ]
