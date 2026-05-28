06f84732d04303111209ddb2954a6f571d8532dc
Madboy <lastsurvivor1017@gmail.com>
2026-05-28 18:16:06 +0500

    paymentsss

--- payments/views.py
+++ payments/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Payment
 from .serializers import PaymentSerializer
+from .services import generate_reference, process_payment
+from django.shortcuts import render
 
-from .services import generate_reference,process_payment
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
     def perform_create(self, serializer):
-        status = process_payment()
-
+        payment_status = process_payment()
         serializer.save(
             reference_id=generate_reference(),
-            status=status
+            status=payment_status
         )
-
 
 def payment(request):
     return render(request, 'payments/payments.html')
